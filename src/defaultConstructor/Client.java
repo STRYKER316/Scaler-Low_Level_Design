8868bd20c280e9e9d4a9e87ba331d58488522c28
Stryker316 <ansil.mishra316@gmail.com>
2023-08-23 09:20:09 +0530

    comment modification

--- src/defaultConstructor/Client.java
+++ src/defaultConstructor/Client.java
@@ -4,6 +4,10 @@ public class Client {
     public static void main(String[] args) {
         Student anshil = new Student();
 
-        System.out.println("Debug");
+//        Every property of the object is initialized to their default values
+        System.out.println(anshil.name);
+        System.out.println(anshil.age);
+        System.out.println(anshil.psp);
+        System.out.println(anshil.universtiyName);
     }
 }
--- src/defaultConstructor/Student.java
+++ src/defaultConstructor/Student.java
@@ -6,5 +6,4 @@ public class Student {
     double psp;
     String universtiyName;
 
-
 }
