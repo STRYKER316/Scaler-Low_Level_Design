7c75787a8b9440461c43b8b09d083d1a668eccf3
Stryker316 <ansil.mishra316@gmail.com>
2023-08-24 12:01:32 +0530

    Code modification

--- src/methodOverloading/test.java
+++ src/methodOverloading/test.java
@@ -6,13 +6,13 @@ public class test {
         return 1;
     }
 
-    int doSmoething(String a) {
-        return 1;
-    }
-
-//    String doSomething() {                // not overloaded, as method have the same signature
+//    String doSomething() {                // not overloaded, as the method have the same signature
 //        System.out.println("Do stuff without params");
 //        return "";
 //    }
 
+    int doSomething(String a) {
+        System.out.println("Do stuff with params");
+        return 1;
+    }
 }
